e0f00a4f9e621a06fc3f7d2ea3f2a954b46e3f79
Cody Jenkins <iknowcss@gmail.com>
2016-01-06 09:19:06 +1100

    Woooohhhhhgit commit -m line

--- src/main.js
+++ src/main.js
@@ -2,24 +2,27 @@
 
 'use strict';
 
+let sq = x => Math.pow(x, 2);
+
 let ball = document.getElementById('ball');
 
 // Initial conditions
-let Y0 = [-150, 0];
+let Y0 = [-.50, 0];
 
 // Coefficients
-let b = 5;
+let b = 0.001;
+let scale = [ 150, 1 ];
 
 // System of equations
 let F = [
   (t, Y) => Y[1],
-  (t, Y) => -100*Y[0] - b*Y[1]
+  (t, Y) => -b*Y[1] - Y[0] + sq(Y[0])
 ];
 
 let tY = [0, Y0];
 setInterval(function () {
-  tY = rk4(F, tY, 0.0001, 100);
-  ball.style.top = (tY[1][0] >> 0) + 'px';
+  tY = rk4(F, tY, 0.001, 100);
+  ball.style.top = ((scale[0] * tY[1][0]) >> 0) + 'px';
 }, 10);
 
 document.addEventListener('click', function () {
