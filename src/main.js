3fde7d12c5c688f9615fb0a2cb1b870e39c8e525
Cody Jenkins <iknowcss@gmail.com>
2016-01-05 23:41:58 +1100

    yesshhhhh

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@
 var ball = document.getElementById('ball');
 
 var F = [
-  (t, Y) => -10 * Y[0]
+  (t, Y) => Y[1],
+  (t, Y) => -200 * Y[0] - 5 * Y[1]
 ];
-var Y0 = [300];
+
+var Y0 = [-150, 0];
 var tY = [0, Y0];
 setInterval(function () {
   tY = rk4(F, tY, 0.0001, 100);
-  ball.style.top = (tY[1][0] >> 0) + 'px';
+  ball.style.top = ((tY[1][0] >> 0) + 150) + 'px';
 }, 10);
 
 document.addEventListener('click', function () {
