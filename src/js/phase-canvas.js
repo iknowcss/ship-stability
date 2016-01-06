c6010f0c0f5f4fb56fdc78462c4a8d4a8de9fc55
CodyJenkins <cody.jenkins@btfinancialgroup.com>
2016-01-06 15:36:24 +1100

    Fix fuzzy line problem

--- src/js/phase-canvas.js
+++ src/js/phase-canvas.js
@@ -2,29 +2,34 @@
 
 'use strict';
 
-window.phaseCanvas = {
-  scale: [1, 1],
+function PhaseCanvas(canvasCtx) {
+  this.scale = [1, 1];
+  this.origin = [150, 75];
+  this.canvasCtx = canvasCtx;
 
-  origin: [150, 75],
+  this.canvasCtx.scale(2, 2);
+}
 
-  canvasCtx: document.getElementById('phase-portrait').getContext('2d'),
-
+Object.assign(PhaseCanvas.prototype, {
   setScale(x, y) {
     this.scale = [x, y];
   },
 
   drawSegment(p0, p1) {
-    this.canvasCtx.beginPath();
-
     let x0 = this.origin[0] + p0[0]*this.scale[0],
         y0 = this.origin[1] + p0[1]*this.scale[1],
         x1 = this.origin[0] + p1[0]*this.scale[0],
         y1 = this.origin[1] + p1[1]*this.scale[1];
+
+    this.canvasCtx.beginPath();
     this.canvasCtx.moveTo(x0, y0);
     this.canvasCtx.lineTo(x1, y1);
-
     this.canvasCtx.stroke();
   }
-};
+});
+
+
+let canvas = document.getElementById('phase-portrait');
+window.phaseCanvas = new PhaseCanvas(canvas.getContext('2d'));
 
 }());
