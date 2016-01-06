1c621469c862991e51b965eeb2767379c22d65f2
CodyJenkins <cody.jenkins@btfinancialgroup.com>
2016-01-06 13:56:41 +1100

    Minor draw code cleanup

--- src/js/phase-canvas.js
+++ src/js/phase-canvas.js
@@ -15,8 +15,14 @@ window.phaseCanvas = {
 
   drawSegment(p0, p1) {
     this.canvasCtx.beginPath();
-    this.canvasCtx.moveTo(this.origin[0] + p0[0]*this.scale[0], this.origin[1] + p0[1]*this.scale[1]);
-    this.canvasCtx.lineTo(this.origin[0] + p1[0]*this.scale[0], this.origin[1] + p1[1]*this.scale[1]);
+
+    let x0 = this.origin[0] + p0[0]*this.scale[0],
+        y0 = this.origin[1] + p0[1]*this.scale[1],
+        x1 = this.origin[0] + p1[0]*this.scale[0],
+        y1 = this.origin[1] + p1[1]*this.scale[1];
+    this.canvasCtx.moveTo(x0, y0);
+    this.canvasCtx.lineTo(x1, y1);
+
     this.canvasCtx.stroke();
   }
 };
