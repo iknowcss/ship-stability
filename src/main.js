9a6a2b8880e46e78cadc8ca244c91299aceb8204
Cody Jenkins <iknowcss@gmail.com>
2016-01-06 11:04:48 +1100

    With art

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ let sq = x => Math.pow(x, 2);
 let ship = document.getElementById('ship');
 
 // Initial conditions
-let Y0 = [-.50, 0];
+let Y0 = [0, -0.72];
 
 // Coefficients
-let b = 0.001;
+let b = 0.1;
 
 // System of equations
 let F = [
@@ -18,16 +18,27 @@ let F = [
   (t, Y) => -b*Y[1] - Y[0] + sq(Y[0])
 ];
 
-let tY = [0, Y0];
-let angle;
-setInterval(function () {
-  tY = rk4(F, tY, 0.001, 100);
-  angle = tY[1][0] * 90;
-  ship.style.transform = `rotate(${angle}deg)`;
-}, 10);
+let simInterval;
+function stopSim() {
+  clearInterval(simInterval);
+}
 
-document.addEventListener('click', function () {
-  tY = [0, Y0];
-});
+function runSim() {
+  stopSim();
+  let tY = [0, Y0];
+  let angle;
+  simInterval = setInterval(function () {
+    tY = rk4(F, tY, 0.001, 100);
+    angle = tY[1][0] * 90;
+    if (angle > 90) {
+      angle = 90;
+      stopSim();
+    }
+    ship.style.transform = `rotate(${angle}deg)`;
+    
+  }, 10);
+}
+
+document.addEventListener('click', runSim);
 
 }());
