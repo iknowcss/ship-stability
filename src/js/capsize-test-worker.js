db1c2c4c8baefa2c5626383a1d7706db44e7b7e1
Cody Jenkins <iknowcss@gmail.com>
2016-01-10 22:05:39 +1100

    Rough worker

--- src/js/capsize-test-worker.js
+++ src/js/capsize-test-worker.js
@@ -1,2 +1,50 @@
 const rk4 = require('./rk4');
+const sq = function (x) { return x*x; };
 
+const enableLogging = true;
+
+function log(a, b) {
+  if (!enableLogging) return;
+  if (b) {
+    console.log(a, b);
+  } else {
+    console.log(a);
+  }
+}
+
+log('worker loaded');
+
+// Coefficients
+var b = 0.01;
+var maxAngle = 100;
+
+onmessage = function (e) {
+  const [w, A, h, steps] = e.data;
+  log('worker receive data:', {w, A, h, steps});
+  const result = isCapsize(w, A, h, steps);
+  log('worker result:', result.capsize ? 'capsize' : 'no capsize');
+  postMessage(result);
+};
+
+function isCapsize(w, A, h, steps) {
+  // System of equations
+  var tY = [0, [0, 0]];
+  var force = function (t) { return A*Math.sin(t); };
+  var F = [
+    function (t, Y) { return Y[1]; },
+    function (t, Y) { return -b*Y[1] - Y[0] + sq(Y[0]) + force(t); }
+  ];
+
+  var angle;
+  var capsize = false;
+  for (var i = 0; i < steps; i++) {
+    tY = rk4(F, tY, h);
+    angle = tY[1][0]*90;
+    if (angle > maxAngle) {
+      capsize = true;
+      break;
+    }
+  }
+
+  return { capsize, steps: i } ;
+}
--- src/js/main-fractal.js
+++ src/js/main-fractal.js
@@ -4,3 +4,55 @@ if (!window.Worker) {
 }
 
 var worker = new Worker('capsize-test-worker.js');
+
+let wDomain = {
+  min: 0,
+  max: 5000,
+  step: 1000
+};
+
+let aDomain = {
+  min: 0,
+  max: 5000,
+  step: 1000
+};
+
+let h = 0.001;
+let steps = 5000;
+
+let wCur;
+let aCur;
+
+function nextPoint() {
+  if (!wCur) wCur = wDomain.min;
+  if (!aCur) aCur = aDomain.min;
+
+  if (wCur < 0 || aCur < 0) {
+    return { w: -1, a: -1 };
+  }
+
+  wCur = wCur + wDomain.step;
+  if (wCur > wDomain.max) {
+    wCur = wDomain.min;
+    aCur = aCur + aDomain.step;
+  }
+
+  if (aCur > aDomain.max) {
+    wCur = undefined;
+    aCur = undefined;
+    return false;
+  }
+  
+  return { w: wCur, a: aCur };
+}
+
+worker.onmessage = function (e) {
+  var { capsize, steps } = e.data;
+  console.log({ capsize, steps });
+};
+
+var testPoint;
+while (testPoint = nextPoint()) {
+  var {w, a} = testPoint;
+  worker.postMessage([ w/1000, a/1000, h, steps ]);
+}
