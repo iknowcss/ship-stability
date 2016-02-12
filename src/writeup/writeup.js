22ccf69deb6a5ab217408049232fba1dcfc08839
Cody Jenkins <iknowcss@gmail.com>
2016-02-13 10:01:40 +1100

    Add some LaTeX

--- src/writeup/writeup.js
+++ src/writeup/writeup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Markdown from 'react-remarkable'
+import Latex from 'react-latex'
 
 import content from 'raw!src/writeup/paragraph/content.md'
 
@@ -13,6 +14,7 @@ export default class Writeup extends Component {
   render () {
     return (
       <div>
+        <Latex>{'Force = $-x + x^{2}$'}</Latex>
         <Markdown options={MD_OPTIONS} source={content}></Markdown>
       </div>
     )
