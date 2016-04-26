29b8da89b3e36cc1d748ca8cc301c7c096483d9a
Cody Jenkins <iknowcss@gmail.com>
2016-04-26 19:44:45 +1000

    Refine explanation

--- src/writeup/Writeup.js
+++ src/writeup/Writeup.js
@@ -113,9 +113,11 @@ export default () => <Markdown options={MD_OPTIONS}>{`
 
   However, in this case our set of equations is non-linear. We can't solve for the boundary directly.
 
-  We will try to find the natural frequency by running some simulations. The interactive graph below contains 25 such simulations. Each ship starts at rest and is rocked by waves of different amplitudes and frequencies.
+  We will try to get a rough picture of the boundry by running some simulations. The interactive grid below contains 25 squares, each one a ship simulation. Each ship starts at rest and is rocked by waves of different amplitudes and frequencies.
 
-  Click <b>Play</b> to start the simulation.
+  Click play <i class="mi mi-play-arrow inline-icon"></i> to start the simulation.
+
+  Toggle the switch to flip between ship mode <i class="mi mi-directions-boat inline-icon"></i> and color mode <i class="mi mi-grid-on inline-icon"></i>.
 
   `}
 
@@ -126,7 +128,19 @@ export default () => <Markdown options={MD_OPTIONS}>{`
       domain={DOMAIN}
     />
     <figcaption>
-      <b>Figure 5</b> - 25 ship stability simulations. In "ship" mode the ship is shown tossing back and forth in the ocean. In "color" mode the phase of the ship is represented by a shade of gray: dark means tipping to the left, light means tipping to the right.
+      <b>Figure 5</b> - 25 ship stability simulations
+      <table>
+        <tbody className="figure-5-table">
+          <tr>
+            <td><i className="mi mi-directions-boat inline-icon"></i></td>
+            <td>In ship mode the ship is shown tossing back and forth in the ocean. When it capsizes the square turns pink.</td>
+          </tr>
+          <tr>
+            <td><i className="mi mi-grid-on inline-icon"></i></td>
+            <td>In color mode the phase of the ship is represented by a shade of gray: dark means tipping to the left, light means tipping to the right. Pink means the ship capsized.</td>
+          </tr>
+        </tbody>
+      </table>
     </figcaption>
   </figure>
 
@@ -138,7 +152,7 @@ export default () => <Markdown options={MD_OPTIONS}>{`
   * Waves with large amplitudes tend to capsize the ship, but not as quickly as we expect
   * **One of the ships that we expect to capsize never does!**
 
-  What happened in the 4th column? The 1st and 3rd ships from the top capsized but the 2nd did not. This means one of our assumptions is wrong. **Large waves do not necessarily capsize the ship**.
+  What happened in the 4th column? The 1st and 3rd ships from the top capsized but the 2nd did not. This means one of our assumptions is wrong. **Larger waves do not necessarily capsize the ship**.
 
   To get a clearer picture of the behavior of ship in different conditions, we will run more simulations. Here is a 64-ship simulation
 
@@ -151,7 +165,7 @@ export default () => <Markdown options={MD_OPTIONS}>{`
       pixelate={true}
     />
     <figcaption>
-      <b>Figure 6</b> - 64 ship stability simulations. For performance reasons we can only view the phase of the ship using colors.
+      <b>Figure 6</b> - 64 ship stability simulations. For performance reasons we can only view simulations in color mode.
     </figcaption>
   </figure>
 
